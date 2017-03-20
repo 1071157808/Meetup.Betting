cf238916daa6593123438d0787339c8e71ee9e0c
andriy.laptyuk <andriy.laptyuk@gmail.com>
2017-03-20 14:41:20 +0200

    1. remove design project 2. fix client and silo start files

--- client.cmd
+++ client.cmd
@@ -1,1 +1,1 @@
-START "Client" /D C:\Dev\dotnet\Meetup.Betting\Meetup.Betting.Client\bin\Debug Meetup.Betting.Client.exe
+START "Client" /D .\Meetup.Betting.Client\bin\Debug Meetup.Betting.Client.exe
--- silo.cmd
+++ silo.cmd
@@ -1,4 +1,4 @@
-START "Silo:9800" /D C:\Dev\dotnet\Meetup.Betting\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9800 proxy=9880
-START "Silo:9801" /D C:\Dev\dotnet\Meetup.Betting\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9801 proxy=9881
-START "Silo:9802" /D C:\Dev\dotnet\Meetup.Betting\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9802 proxy=9882
-START "Silo:9803" /D C:\Dev\dotnet\Meetup.Betting\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9803 proxy=9883
+START "Silo:9800" /D .\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9800 proxy=9880
+START "Silo:9801" /D .\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9801 proxy=9881
+START "Silo:9802" /D .\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9802 proxy=9882
+START "Silo:9803" /D .\Meetup.Betting.Host\bin\Debug Meetup.Betting.Host.exe silo=9803 proxy=9883
